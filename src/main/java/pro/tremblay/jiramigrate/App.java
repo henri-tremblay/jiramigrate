4d252db7e7fab95981c3cd0918696191e8a9a9a1
Henri Tremblay <henri.tremblay@gmail.com>
2015-05-21 16:15:20 -0400

    Initial generation

---
+++ src/main/java/pro/tremblay/jiramigrate/App.java
@@ -0,0 +1,13 @@
+package pro.tremblay.jiramigrate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
